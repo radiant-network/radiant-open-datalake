32e02cddccaccd4e0dc12776ce3c6242c6f240c9
Karl Ritchie <ritchie.karl@gmail.com>
2026-04-20 16:16:50 -0400

    feat: sjra-1341 fix dag tests

--- airflow/tests/unit/dags/test_discover_new_source_versions.py
+++ airflow/tests/unit/dags/test_discover_new_source_versions.py
@@ -8,5 +8,6 @@ def test_dag_has_expected_tasks(dag_bag):
     dag = dag_bag.get_dag(dag_id="opendatalake-discover-new-source-versions")
     expected_tasks = {
         "clinvar_check_for_update",
+        "dbsnp_check_for_update",
     }
     assert set(dag.task_ids) == expected_tasks
